c1a5002b2c321ccbe5a71c19a78ff2b34ecec186
hollszabolcs <hollszabolcs@gmail.com>
2023-06-08 17:45:37 +0200

    Day Of Months

---
+++ DayOfMonths/src/DayOfMonths.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DayOfMonths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please give me a number");
+        byte month = scanner.nextByte();
+        switch (month) {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("This month have 30 days");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("This month have 31 days");
+                break;
+            case 2:
+                System.out.println("This month have 28/29 days");
+                break;
+            default:
+                System.err.println("number is out of range");
+                break;
+
+        }
+        scanner.close();
+    }
+
+}
