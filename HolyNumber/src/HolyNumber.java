66a4e37904c65515dc9a057b064aeba2a258c931
hollszabolcs <hollszabolcs@gmail.com>
2023-06-21 22:45:31 +0200

    Mennyei szam hazi

---
+++ HolyNumber/src/HolyNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HolyNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please give me a number");
+        int number = scanner.nextInt();
+        int counter = 0;
+        if (number < 100 && number % 7 != 0 && number % 2 == 0) {
+            System.out.println("A(z) " + number + " Mennyei szám");
+        } else if (number > 300 && number % 13 == 0) {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    counter += 1;
+                }
+                if (counter == 8) {
+                    System.out.println("A(z) " + number + " Mennyei szám");
+                    break;
+                } else if (counter != 8) {
+                    continue;
+                } else {
+                    System.out.println("A(z) " + number + " NEM Mennyei szám");
+                    break;
+                }
+            }
+        } else {
+            System.out.println("A(z) " + number + " NEM Mennyei szám");
+        }
+    }
+}
+
