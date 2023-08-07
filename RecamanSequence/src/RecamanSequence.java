7bf5b2505e3629cd3a67a3aac79bb4d774f33118
hollszabolcs <hollszabolcs@gmail.com>
2023-08-07 20:13:37 +0200

    My first version of Recaman Sequence homework

--- RecamanSequence/src/RecamanSequence.java
+++ RecamanSequence/src/RecamanSequence.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class RecamanSequence {
+import java.util.Scanner;
+
+public class RecamanSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How long want do you go in recaman sequence?");
+        int number = scanner.nextInt();
+        int recaman = 0;
+        int[] array = new int[number];
+        for (int i = 0; i < number; i++) {
+            if (recaman - i > 0) {
+                recaman -= i;
+                for (int recamannumber : array) {
+                    if (recamannumber == recaman) {
+                        recaman += i;
+                    }
+                }
+                array[i] = recaman;
+                System.out.println(array[i]);
+            } else {
+                recaman += i;
+                array[i] = recaman;
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
