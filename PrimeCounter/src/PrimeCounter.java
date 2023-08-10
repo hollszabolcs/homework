af0b6ceb3ec7da3b486fbc3c3b662ee532ac511c
hollszabolcs <hollszabolcs@gmail.com>
2023-08-10 19:17:52 +0200

    First exercies of small projects

---
+++ PrimeCounter/src/PrimeCounter.java
@@ -0,0 +1,29 @@
+//Elso feladat
+public class PrimeCounter {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1000; i <= 2073; i++) {
+            if (isPrime(i) == true) {
+                counter += 1;
+                System.out.println(i);
+            }
+        }
+        System.out.println("there is " + counter + " prime number");
+    }
+
+
+    static boolean isPrime(int number) {
+        if (number < 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
