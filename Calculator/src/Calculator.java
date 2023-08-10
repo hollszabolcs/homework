933842170da825db1b12ad3807dcabad0171f0ba
hollszabolcs <hollszabolcs@gmail.com>
2023-08-10 19:11:36 +0200

    Nameing convension and logical falilures fix and optimazition

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -17,40 +17,39 @@ public class Calculator {
         int[] operands = new int[2];
         int operand = 0;
         if (number <= 7) {
-            operands = getOperands(scanner);
+            operands = getOneOperands(scanner);
         } else {
-            operand = getOneoperand(scanner);
+            operand = getOneOperand(scanner);
         }
         scanner.close();
         switch (number) {
             case 1:
-                getSum(operands);
+                System.out.println(getSum(operands));
                 break;
             case 2:
-                getDif(operands);
+                System.out.println(getDif(operands));
                 break;
             case 3:
-                getMulti(operands);
+                System.out.println(getMulti(operands));
                 break;
             case 4:
-                getDivision(operands);
+                System.out.println(getDivision(operands));
                 break;
             case 5:
-                getModularDiv(operands);
+                System.out.println(getModularDiv(operands));
                 break;
             case 6:
-                getPower(operands);
+                System.out.println(getPower(operands));
                 break;
             case 7:
-                getPercentage(operands);
+                System.out.println(getPercentage(operands));
                 break;
             case 8:
-                getFactorial(operand);
+                System.out.println(getFactorial(operand));
                 break;
             case 9:
-                getSquareRoot(operand);
+                System.out.println(getSquareRoot(operand));
                 break;
-            default:
 
         }
     }
@@ -59,8 +58,7 @@ public class Calculator {
         return scanner.nextInt();
     }
 
-
-    private static int[] getOperands(Scanner scanner) {
+    private static int[] getOneOperands(Scanner scanner) {
         int[] operands = new int[2];
         System.out.println("Please type two numbers");
         for (int i = 0; i < operands.length; ) {
@@ -70,94 +68,60 @@ public class Calculator {
         return operands;
     }
 
-    private static int getOneoperand(Scanner scanner) {
-        int operator = 0;
+    private static int getOneOperand(Scanner scanner) {
+        int operand = 0;
         System.out.println("Please type one number");
-        operator = scanner.nextInt();
-        return operator;
+        operand = scanner.nextInt();
+        return operand;
     }
 
     static int getSum(int[] numbers) {
         int result = 0;
-        for (int operators : numbers) {
-            result += operators;
+        for (int values : numbers) {
+            result += values;
         }
-        System.out.println(result);
         return result;
     }
 
     static int getDif(int[] numbers) {
-        int number1 = 0;
-        int number2 = 0;
-        int i = 0;
-        for (int operators : numbers) {
-            number1 = numbers[i];
-            i++;
-            number2 = numbers[i];
-            break;
-        }
-
+        int number1 = numbers[0];
+        int number2 = numbers[1];
         int result = number1 - number2;
-        System.out.println(result);
         return result;
     }
 
     static double getMulti(int[] numbers) {
-        double number1 = 0;
-        double number2 = 0;
-        int i = 0;
-        for (int operators : numbers) {
-            number1 = numbers[i];
-            i++;
-            number2 = numbers[i];
-            break;
-        }
+        double number1 = numbers[0];
+        double number2 = numbers[1];
         double result = number1 * number2;
-        System.out.println(result);
         return result;
     }
 
     static double getDivision(int[] numbers) {
-        int number1 = 0;
-        int number2 = 0;
-        for (int i = 0; i < numbers.length; ) {
-            number1 = numbers[i];
-            i++;
-            number2 = numbers[i];
-            break;
-        }
+        double number1 = numbers[0];
+        double number2 = numbers[1];
         double result = number1 / number2;
-        System.out.println(result);
         return result;
     }
 
     static double getModularDiv(int[] numbers) {
-        int number1 = 0;
-        int number2 = 0;
-        for (int i = 0; i < numbers.length; ) {
-            number1 = numbers[i];
-            i++;
-            number2 = numbers[i];
-            break;
-        }
+        double number1 = numbers[0];
+        double number2 = numbers[1];
         double result = number1 % number2;
-        System.out.println(result);
         return result;
     }
 
     static long getPower(int[] numbers) {
         double number = numbers[0];
         double pow = numbers[1];
-        double pownumber = Math.pow(number, pow);
-        System.out.println((int) pownumber);
-        return (long) pownumber;
+        double powNumber = Math.pow(number, pow);
+        return (long) powNumber;
     }
 
     static double getPercentage(int[] numbers) {
         double number1 = numbers[0];
         double number2 = numbers[1];
         double result = (number2 / number1) * 100;
-        System.out.println(result);
         return result;
     }
 
@@ -166,13 +130,11 @@ public class Calculator {
         for (int i = 2; i <= num; i++) {
             factorial *= i;
         }
-        System.out.println(factorial);
         return factorial;
     }
 
     static double getSquareRoot(int num) {
         double square = Math.sqrt(num);
-        System.out.println(square);
         return square;
     }
 }
