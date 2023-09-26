15a4f833e482dd7b19c3fa71a08c246bc55773e6
hollszabolcs <hollszabolcs@gmail.com>
2023-09-26 18:58:24 +0200

    test

--- OOP/src/occupations/Main.java
+++ OOP/src/occupations/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person(42,"Gabor",Gender.MAN);
         person.introduce();
+        person.printGoal();
          Chef chef = new Chef(42,"Gabor",Gender.MAN);
          chef.introduce();
         chef.cook("broccoli");
