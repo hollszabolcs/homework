611a09a85725f0f53eda36df1447e0cc1bf75353
hollszabolcs <hollszabolcs@gmail.com>
2023-09-26 21:10:45 +0200

    Editing test

--- Gitteszt3/src/Gitteszt3.java
+++ Gitteszt3/src/Gitteszt3.java
@@ -1,5 +1,20 @@
 public class Gitteszt3 {
     public static void main(String[] args) {
         System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
+        System.out.println("sdgfdgdfdgd321");
     }
 }
