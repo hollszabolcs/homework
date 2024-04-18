b0374a4c2a18c7f0114660628ca26e4e02e3e00f
hollszabolcs <hollszabolcs@gmail.com>
2024-04-18 17:33:10 +0200

    I made the changes due the suggestions

--- LoggerInterface/src/FileLogger.java
+++ LoggerInterface/src/FileLogger.java
@@ -1,13 +1,24 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileLogger implements Logger {
     @Override
     public void log(String messeage) {
-        try (FileWriter writer = new FileWriter("C:/log.txt")) {
+        System.out.println("Please give a file name");
+        String fileName=getText();
+        System.out.println("Please give a message");
+        messeage=getText();
+        try (FileWriter writer = new FileWriter("C:/"+fileName+".txt")) {
             writer.write(messeage);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getText(){
+        Scanner scanner=new Scanner(System.in);
+        String text = scanner.nextLine();
+        return  text;
     }
 }
--- LoggerInterface/src/Main.java
+++ LoggerInterface/src/Main.java
@@ -6,4 +6,3 @@ public class Main {
         filelogger.log("Test message");
     }
 }
-Lo  
