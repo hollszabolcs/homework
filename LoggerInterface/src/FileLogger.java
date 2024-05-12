ec55f316995a0e4c6eb458ff252e5d0c2d48525e
hollszabolcs <hollszabolcs@gmail.com>
2024-05-12 20:12:20 +0200

    Revisited homework

--- LoggerInterface/src/FileLogger.java
+++ LoggerInterface/src/FileLogger.java
@@ -7,9 +7,11 @@ public class FileLogger implements Logger {
     public void log(String messeage) {
         System.out.println("Please give a file name");
         String fileName=getText();
+        System.out.println("please give me the saving path");
+        String filePath=getText();
         System.out.println("Please give a message");
         messeage=getText();
-        try (FileWriter writer = new FileWriter("C:/"+fileName+".txt")) {
+        try (FileWriter writer = new FileWriter(filePath+fileName+".txt")) {
             writer.write(messeage);
         } catch (IOException e) {
             e.printStackTrace();
